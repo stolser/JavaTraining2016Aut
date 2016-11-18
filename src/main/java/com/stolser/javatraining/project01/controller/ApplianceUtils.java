3cc285414714b4500837113d90742f95bec84292
Stoliarov Oleg <stoljarov.oleg@gmail.com>
2016-11-18 11:11:05 +0200

    Change comparator impl for sorting algorithms

--- src/main/java/com/stolser/javatraining/project01/controller/ApplianceUtils.java
+++ src/main/java/com/stolser/javatraining/project01/controller/ApplianceUtils.java
@@ -11,19 +11,21 @@ public class ApplianceUtils {
 
     static {
         algorithms = new HashMap<>();
-        algorithms.put(BY_PRICE_ASC, (first, second) -> Double.compare(first.getPrice(), second.getPrice()));
-        algorithms.put(BY_PRICE_DESC, (first, second) -> Double.compare(second.getPrice(), first.getPrice()));
-        algorithms.put(BY_POWER_ASC, (first, second) -> Double.compare(first.getMaxPower(), second.getMaxPower()));
-        algorithms.put(BY_POWER_DESC, (first, second) -> Double.compare(second.getMaxPower(), first.getMaxPower()));
-        algorithms.put(BY_WEIGHT_ASC, (first, second) -> Double.compare(first.getWeight(), second.getWeight()));
-        algorithms.put(BY_WEIGHT_DESC, (first, second) -> Double.compare(second.getWeight(), first.getWeight()));
+        algorithms.put(BY_PRICE_ASC, (first, second) -> (first.getPrice() < second.getPrice()) ? -1 : 1);
+        algorithms.put(BY_PRICE_DESC, (first, second) -> (second.getPrice() < first.getPrice()) ? -1 : 1);
+        algorithms.put(BY_POWER_ASC, (first, second) -> (first.getMaxPower() < second.getMaxPower()) ? -1 : 1);
+        algorithms.put(BY_POWER_DESC, (first, second) -> (second.getMaxPower() < first.getMaxPower()) ? -1 : 1);
+        algorithms.put(BY_WEIGHT_ASC, (first, second) -> (first.getWeight() < second.getWeight()) ? -1 : 1);
+        algorithms.put(BY_WEIGHT_DESC, (first, second) -> (second.getWeight() < first.getWeight()) ? -1 : 1);
     }
 
-    public static Set<ElectricalAppliance> getSorted(Set<ElectricalAppliance> unsortedElems,
+    public static NavigableSet<ElectricalAppliance> getSorted(Set<ElectricalAppliance> unsortedElems,
                                                                     SortingType algorithm) {
         Comparator<ElectricalAppliance> comparator = algorithms.get(algorithm);
-        Set<ElectricalAppliance> sortedElems = new TreeSet<>(comparator);
-
+        NavigableSet<ElectricalAppliance> sortedElems = new TreeSet<>(comparator);
+        System.out.println("-----------UnsortedSet----------");
+        unsortedElems.forEach(System.out::println);
+        System.out.println("---------------------------------");
         sortedElems.addAll(unsortedElems);
 
         return sortedElems;
